e41659451a731651db5befd0d4380e78d4c3be36
Sasha-Gill <54946194+Sasha-Gill@users.noreply.github.com>
2020-03-19 16:20:33 +0100

    punkt 1-4 avklarad

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -6,7 +6,37 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            if (6>3)
+            { 
+            Console.WriteLine("Hello, World!");
+                Console.ReadLine();
+            }
+
+            string Name;
+            string Password;
+            Console.WriteLine("Write your name: ");
+            Name = Console.ReadLine();
+            Console.WriteLine("Write your password: ");
+            Password = Console.ReadLine();
+
+            if (Name == "noname" && Password == "nopass")
+            {
+                Console.WriteLine("Welcome!");
+            }
+            else 
+            {
+                Console.WriteLine("Wrong username or password!");
+            }
+
+            int i = 0;
+            
+            while (32>i)
+            {
+                Console.WriteLine("Hello, World "+i); //lade till + i för att kunna se nummer på varje mening.
+                i++;
+            }
+
+
         }
     }
 }
